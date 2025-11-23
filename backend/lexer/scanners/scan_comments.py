b8f04dfb936dd6585a6a689c71d49e223fc6096d
kaelthas <jmvl.academic@gmail.com>
2025-11-24 02:02:31 +0800

    no changes

--- backend/lexer/scanners/scan_comments.py
+++ backend/lexer/scanners/scan_comments.py
@@ -4,31 +4,43 @@ from backend.lexer.errors import LexicalError
 def scan_comment(lexer):
     pos = lexer.pos.copy()
 
+    # multi line comment
     if lexer.current_char == "/" and lexer.peek() == "/":
         start_pos = lexer.pos.copy()
         lexeme = "//"
         lexer.advance()
         lexer.advance()
+
         while lexer.current_char is not None and lexer.current_char != "\n":
             lexeme += lexer.current_char
             lexer.advance()
+
         return Token(TK_COMMENT, lexeme, start_pos)
+
+    # multi line comment
     if lexer.current_char == "/" and lexer.peek() == "*":
         start_pos = lexer.pos.copy()
         lexeme = "/*"
         lexer.advance()
         lexer.advance()
+
         while True:
             if lexer.current_char is None:
                 raise LexicalError(pos, "Unterminated block comment")
+
+            if lexer.current_char == "/" and lexer.peek() == "*":
+                raise LexicalError(pos, "Nested block comments are not allowed")
+
             if lexer.current_char == "*" and lexer.peek() == "/":
                 lexeme += "*"
                 lexer.advance()
                 lexeme += "/"
                 lexer.advance()
                 break
+
             lexeme += lexer.current_char
             lexer.advance()
+
         return Token(TK_COMMENT_BLOCK, lexeme, start_pos)
-    
+
     raise LexicalError(pos, "Not a comment")
