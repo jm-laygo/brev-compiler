f07ae218ff1d56c99e3430d87bbd24c934caba0c
kaelthas <jmvl.academic@gmail.com>
2025-11-28 04:51:25 +0800

    fixed the max identifier length not giving an error when exceeded

--- backend/lexer/scanners/scan_identifiers.py
+++ backend/lexer/scanners/scan_identifiers.py
@@ -14,27 +14,22 @@ def scan_identifier(lexer, allow_reserved_as_type=True):
 
     while lexer.current_char is not None:
         if lexer.current_char in id_sym:
-            if len(text) < MAX_IDENTIFIER_LENGTH:
-                text += lexer.current_char
+            text += lexer.current_char
             lexer.advance()
             continue
         if lexer.current_char in idnt_delim or lexer.current_char == "\n":
             break
         raise LexicalError(pos, f"Invalid character '{lexer.current_char}' in identifier '{text}'")
-    
+
+    # Check for max length
+    if len(text) > MAX_IDENTIFIER_LENGTH:
+        raise LexicalError(pos, f"Identifier '{text}' exceeds maximum length of {MAX_IDENTIFIER_LENGTH} characters")
+
+    # Reserved word handling
     if text in RESERVED_WORDS:
         if allow_reserved_as_type:
             return Token(RESERVED_WORDS[text], text, pos)
         else:
             raise LexicalError(pos, f"Reserved word '{text}' cannot be used as identifier")
-    
-    for kw in RESERVED_WORDS:
-        if text.startswith(kw) and text != kw:
-            nxt = lexer.current_char
-            if nxt is not None and nxt.isalpha():
-                raise LexicalError(pos, f"Illegal continuation after reserved word '{kw}' in '{text}'")
-            
-    if lexer.current_char is not None and lexer.current_char not in idnt_delim and lexer.current_char != "\n":
-        raise LexicalError(pos, f"Invalid character '{lexer.current_char}' in identifier '{text}'")
 
     return Token(TK_IDENTIFIER, text, pos)
