39f65c65021f82f99f22f0a8122d3cd023bbf4ab
kaelthas <jmvl.academic@gmail.com>
2025-11-28 04:50:58 +0800

    fix the error line with unclosed and nested

--- backend/lexer/scanners/scan_comments.py
+++ backend/lexer/scanners/scan_comments.py
@@ -2,35 +2,32 @@ from backend.lexer.tokens import Token, TK_COMMENT, TK_COMMENT_BLOCK
 from backend.lexer.errors import LexicalError
 
 def scan_comment(lexer):
-    pos = lexer.pos.copy()
+    start_pos = lexer.pos.copy()
 
-    # multi line comment
+    # SINGLELINE COMMENT
     if lexer.current_char == "/" and lexer.peek() == "/":
-        start_pos = lexer.pos.copy()
         lexeme = "//"
         lexer.advance()
         lexer.advance()
-
         while lexer.current_char is not None and lexer.current_char != "\n":
             lexeme += lexer.current_char
             lexer.advance()
-
         return Token(TK_COMMENT, lexeme, start_pos)
 
-    # multi line comment
+    # MULTILINE COMMENT
     if lexer.current_char == "/" and lexer.peek() == "*":
-        start_pos = lexer.pos.copy()
         lexeme = "/*"
         lexer.advance()
         lexer.advance()
 
         while True:
             if lexer.current_char is None:
-                raise LexicalError(pos, "Unterminated block comment")
+                raise LexicalError(start_pos, "Unterminated block comment")
 
             if lexer.current_char == "/" and lexer.peek() == "*":
-                raise LexicalError(pos, "Nested block comments are not allowed")
-
+                err_pos = lexer.pos.copy()
+                raise LexicalError(err_pos, "Nested block comments are not allowed")
+            
             if lexer.current_char == "*" and lexer.peek() == "/":
                 lexeme += "*"
                 lexer.advance()
@@ -43,4 +40,4 @@ def scan_comment(lexer):
 
         return Token(TK_COMMENT_BLOCK, lexeme, start_pos)
 
-    raise LexicalError(pos, "Not a comment")
+    raise LexicalError(lexer.pos.copy(), "Not a comment")
